c1b7006af8a3da4f2c0a94b54928079e9a091b9b
Subramanya Sastry <sss.lists@gmail.com>
2016-03-08 15:02:48 -0600

    Tweaks to testreduce title generation scripts
    * Added ptwiki, 2 more wikitionaries, wikisource, wikivoyage.
    * Some minor cleanup.
    * Updated numbers to generate fewer total (~65K) # of titles
      for visual diff tests.

--- server/scripts/download.sh
+++ server/scripts/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #---- for wikis ---
-LANG="en de nl fr it ru es sv pl ja ar he hi ko zh"
+LANG="en de nl fr it ru es sv pl pt ja ar he hi ko zh"
 # link prefix languages
 LANG=$LANG" ckb cu cv hy is kaa ka lbe ln mzn pnb uk uz"
 
@@ -10,7 +10,19 @@ for l in $LANG ; do
 done
 
 #---- for wiktionaries ---
-LANG="en fr"
+LANG="en fr it es"
 for l in $LANG ; do
     wget http://dumps.wikimedia.org/${l}wiktionary/latest/${l}wiktionary-latest-all-titles-in-ns0.gz
+done
+
+#---- for wikisource ---
+LANG="en fr it es"
+for l in $LANG ; do
+    wget http://dumps.wikimedia.org/${l}wikisource/latest/${l}wikisource-latest-all-titles-in-ns0.gz
+done
+
+#---- for wikivoyage ---
+LANG="en fr it es"
+for l in $LANG ; do
+    wget http://dumps.wikimedia.org/${l}wikivoyage/latest/${l}wikivoyage-latest-all-titles-in-ns0.gz
 done
