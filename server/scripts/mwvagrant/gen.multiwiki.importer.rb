c385f0abfadaf4e05d477853a842a6348d6d05c9
Subramanya Sastry <sss.lists@gmail.com>
2016-04-09 15:36:49 -0500

    More tweaks to the mwvagrant scripts

--- server/scripts/mwvagrant/gen.multiwiki.importer.rb
+++ server/scripts/mwvagrant/gen.multiwiki.importer.rb
@@ -9,12 +9,12 @@ wikis.each do |s|
    dbname = "#{s}wiki".sub(/wikiwiki/, 'wiki')
    puts <<EOF
 echo "------------- #{s} --------------"
-curl 'http://dumps.wikimedia.org/other/testfiles/20160405/#{s}_20160405_testsamples.xml.bz2' -o - | bunzip2 > ../#{s}.dump.xml
+curl 'https://dumps.wikimedia.org/other/testfiles/20160405/#{s}_20160405_testsamples.xml.bz2' -o - | bunzip2 > ../#{s}.dump.xml
 echo "Number of titles to import: " `grep "<title" ../#{s}.dump.xml | wc -l`
 mwscript maintenance/update.php --wiki #{dbname}
 mwscript maintenance/importDump.php --wiki #{dbname} < ../#{s}.dump.xml
 rm ../#{s}.dump.xml
-mwscript maintenance/rebuildrecentchanges.php
+mwscript maintenance/rebuildrecentchanges.php --wiki #{dbname}
 rm ../logs/mediawiki-#{dbname}-debug.log
 df
 EOF
