641a83e315756e04a3e122fd62894862c6e2b1ac
Subramanya Sastry <ssastry@wikimedia.org>
2024-03-11 13:30:28 -0500

    Tweak the title generator to not rely only on RC stream for talk ns
    The new strategy is not foolproof, since, as I note in the comments,
    the talk ns titles might not exist (since not all articles have an
    associated talk page -- especially on less popular wikis). So, for
    those the RC stream OR looking at quarry might be better!
    
    Change-Id: I9f5ea9ddd3ad422004502c7b7b8f2aa19cb3b0f6

--- server/scripts/gen_titles.js
+++ server/scripts/gen_titles.js
@@ -8,7 +8,7 @@ const testdb = require('./testdb.info.js');
 const wikis = testdb.wikis;
 const Promise = require('prfun/wrap')(require('babybird'));
 const execP = Promise.promisify(cp.exec, ['stdout', 'stderr'], cp);
-const forceDumpsRefresh = false;
+const forceDumpsRefresh = true;
 
 function generate_titles() {
 	const sum = wikis.reduce(function(s, w) {
@@ -17,7 +17,7 @@ function generate_titles() {
 	Promise.reduce(wikis, function(unused, w) {
 		const wiki = w.prefix;
 		const isTalk = w.ns === 1;
-		const fraction = isTalk ? 1 : testdb.dump_percentage / 100;
+		const fraction = testdb.dump_percentage / 100;
 		const wikiWithNS = isTalk ? `${ wiki }_talk` : wiki;
 		const n = Math.ceil(w.limit / sum * fraction * testdb.size);
 		const total = Math.ceil(n / fraction);
@@ -25,28 +25,39 @@ function generate_titles() {
 
 		console.log(`--- wiki ${ wikiWithNS } ----`);
 		console.log(`Generating ${ total } titles in all`);
-		if (isTalk) {
+		const file = `${ wiki }.random_titles.txt`;
+		const dumpFile = `dumps/${ wiki }-latest-all-titles-in-ns0.gz`;
+		if (forceDumpsRefresh || !fs.existsSync(dumpFile)) {
 			commands = [
-				`cp ${ wikiWithNS }.rc_titles.txt ${ wikiWithNS }.all_titles.txt`
+				`wget http://dumps.wikimedia.org/${ wiki }/latest/${ wiki }-latest-all-titles-in-ns0.gz`,
+				`mv *.gz dumps/`
 			];
 		} else {
-			const file = `${ wiki }.random_titles.txt`;
-			const dumpFile = `dumps/${ wiki }-latest-all-titles-in-ns0.gz`;
-			if (forceDumpsRefresh || !fs.existsSync(dumpFile)) {
-				commands = [
-					`wget http://dumps.wikimedia.org/${ wiki }/latest/${ wiki }-latest-all-titles-in-ns0.gz`,
-					`mv *.gz dumps/`
-				];
-			} else {
-				commands = [];
-			}
-			commands = commands.concat([
-				`zcat ${ dumpFile } | shuf | head -${ n } > ${ file }`,
-				`head -2 ${ file }`,
-				`cat ${ file } ${ wiki }.top_titles.txt ${ wikiWithNS }.rc_titles.txt | sort | uniq | shuf | head -${ total } > ${ wikiWithNS }.all_titles.txt`,
-			]);
-			console.log(`Generating ${ n } random titles from dump`);
+			commands = [];
 		}
+		commands = commands.concat([
+			`zcat ${ dumpFile } | shuf | head -${ n } > ${ file }`,
+			`head -2 ${ file }`,
+		]);
+		if (isTalk) {
+			// NOTE: Not all titles here might exist - just a workaround
+			// to having to download *all* titles.
+			commands = commands.concat([
+				`sed 's/^/Talk:/g;' < ${ file } > ${ file }.talkns`,
+				`mv -f ${ file }.talkns ${ file }`,
+			]);
+		}
+
+		commands = commands.concat([
+			// This will emit less than 'total' titles because of
+			// - we may not have (100 - testdb.dump_percentage)% titles from RC
+			// - title dupes between the two files
+			// Additionally, in the case of talk ns, since not all talk:* titles from
+			// dumps might exist, the actual working set once we run the first test run
+			// will be smaller.
+			`cat ${ wiki }.top_titles.txt ${ wikiWithNS }.rc_titles.txt ${ file } | sort | uniq | shuf | head -${ total } > ${ wikiWithNS }.all_titles.txt`,
+		]);
+		console.log(`Generating ${ n } random titles from dump`);
 
 		return execP(commands.join("; ")).then(function(out) {
 			console.log(out.stdout);
