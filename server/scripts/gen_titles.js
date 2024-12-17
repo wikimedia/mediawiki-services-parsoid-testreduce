a41b7606ddb8ca817e8fc72fc89069ce743efeef
Subramanya Sastry <ssastry@wikimedia.org>
2024-12-17 11:42:36 -0600

    Adapt title generation script for all namespaces & wiktionary
    * Pick 11 wiktionaries to start with and 50K titles in all
    * Fetch all titles from dumps (not just ns0)
    * Introduce namespace mapping to generate namespace-resolved titles
    
    Change-Id: I2c314d862cf23e155c6e258d2c8a8ed8a2aab9c0

--- server/scripts/gen_titles.js
+++ server/scripts/gen_titles.js
@@ -8,7 +8,27 @@ const testdb = require('./testdb.info.js');
 const wikis = testdb.wikis;
 const Promise = require('prfun/wrap')(require('babybird'));
 const execP = Promise.promisify(cp.exec, ['stdout', 'stderr'], cp);
-const forceDumpsRefresh = true;
+const forceDumpsRefresh = false;
+const NS_MAP = {
+	0:"",
+	2:"User",
+	4:"Project",
+	6:"File",
+	8:"MediaWiki",
+	10:"Template",
+	12:"Help",
+	14:"Category",
+	90:"Thread", // LQT
+	92:"Summary", // LQT
+	100:"Appendix", // wiktionary-specific
+	106:"Rhymes", // wiktonary-specific
+	108:"Transwiki", // wiktionary-specific
+	110:"Thesaurus", // wiktionary-specific
+	114:"Citations", // wiktionary-specific
+	116:"Sign gloss", // wiktionary-specific
+	118:"Reconstruction", // wiktionary-specific
+	828:"Module",
+};
 
 function generate_titles() {
 	const sum = wikis.reduce(function(s, w) {
@@ -16,62 +36,73 @@ function generate_titles() {
 	}, 0);
 	Promise.reduce(wikis, function(unused, w) {
 		const wiki = w.prefix;
-		const isTalk = w.ns === 1;
 		const fraction = testdb.dump_percentage / 100;
-		const wikiWithNS = isTalk ? `${ wiki }_talk` : wiki;
+		const wikiWithNS = wiki;
 		const n = Math.ceil(w.limit / sum * fraction * testdb.size);
 		const total = Math.ceil(n / fraction);
-		let commands;
+		let dumpCommands;
 
 		console.log(`--- wiki ${ wikiWithNS } ----`);
 		console.log(`Generating ${ total } titles in all`);
-		const file = `${ wiki }.random_titles.txt`;
-		const dumpFile = `dumps/${ wiki }-latest-all-titles-in-ns0.gz`;
+		const randTitlesFile = `${ wiki }.random_titles.txt`;
+		const dumpFile = `dumps/${ wiki }-latest-all-titles.gz`;
 		if (forceDumpsRefresh || !fs.existsSync(dumpFile)) {
-			commands = [
-				`wget http://dumps.wikimedia.org/${ wiki }/latest/${ wiki }-latest-all-titles-in-ns0.gz`,
+			dumpCommands = [
+				`wget http://dumps.wikimedia.org/${ wiki }/latest/${ wiki }-latest-all-titles.gz`,
 				`mv *.gz dumps/`
 			];
 		} else {
-			commands = [];
+			dumpCommands = [];
 		}
-		commands = commands.concat([
-			`zcat ${ dumpFile } | shuf | head -${ n } > ${ file }`,
-			`head -2 ${ file }`,
+		dumpCommands = dumpCommands.concat([
+			`zcat ${ dumpFile } | shuf | head -${ n } > ${ randTitlesFile }`,
+			`head -2 ${ randTitlesFile }`,
 		]);
-		if (isTalk) {
-			// NOTE: Not all titles here might exist - just a workaround
-			// to having to download *all* titles.
-			commands = commands.concat([
-				`sed 's/^/Talk:/g;' < ${ file } > ${ file }.talkns`,
-				`mv -f ${ file }.talkns ${ file }`,
-			]);
-		}
-
-		commands = commands.concat([
-			// This will emit less than 'total' titles because of
-			// - we may not have (100 - testdb.dump_percentage)% titles from RC
-			// - title dupes between the two files
-			// Additionally, in the case of talk ns, since not all talk:* titles from
-			// dumps might exist, the actual working set once we run the first test run
-			// will be smaller.
-			`cat ${ wiki }.top_titles.txt ${ wikiWithNS }.rc_titles.txt ${ file } | sort | uniq | shuf | head -${ total } > ${ wikiWithNS }.all_titles.txt`,
-		]);
-		console.log(`Generating ${ n } random titles from dump`);
-
-		return execP(commands.join("; ")).then(function(out) {
+		return execP(dumpCommands.join("; ")).then(function(out) {
 			console.log(out.stdout);
-			const titles = fs.readFileSync(`${ wikiWithNS }.all_titles.txt`, 'utf8').split(/[\n\r]+/);
-			out = [];
-			for (let i = 0; i < titles.length; i++) {
-				const t = titles[i].replace(/"/g, '\\"');
-				if (t) {
-					out.push(`INSERT IGNORE INTO pages(title, prefix) VALUES("${ t }", "${ wiki }");`);
+			const dumpTitles = fs.readFileSync(randTitlesFile, 'utf8').split(/[\n]/);
+			const resolvedTitles = [];
+			for (let i = 0; i < dumpTitles.length; i++) {
+				const t = dumpTitles[i].split(/\t/);
+				const nsId = Number(t[0]);
+				let ns;
+				if (nsId % 2 === 0) {
+					ns = NS_MAP[String(nsId)];
+					ns = ns ? ns + ':' : '';
+				} else {
+					ns = NS_MAP[String(nsId - 1)];
+					ns = ns + (ns ? '_' : '') + 'Talk:';
 				}
+				resolvedTitles.push(ns + t[1]);
 			}
-			fs.writeFileSync(`${ wikiWithNS }.titles.sql`, out.join("\n"));
-			console.log(`Generated sql import script @ ${ wikiWithNS }.titles.sql`);
-			return true;
+			fs.writeFileSync(randTitlesFile, resolvedTitles.join('\n'));
+			const commands = [
+				// This will emit less than 'total' titles because of
+				// - we may not have (100 - testdb.dump_percentage)% titles from RC
+				// - title dupes between the two files
+				// Additionally, in the case of talk ns, since not all talk:* titles from
+				// dumps might exist, the actual working set once we run the first test run
+				// will be smaller.
+				`cat ${ wiki }.top_titles.txt ${ wikiWithNS }.rc_titles.txt ${ randTitlesFile } | sort | uniq | shuf | head -${ total } > ${ wikiWithNS }.all_titles.txt`,
+			];
+			console.log(`Generating ${ n } random titles from dump`);
+
+			execP(commands.join("; ")).then(function(out2) {
+				console.log(out2.stdout);
+				const titles = fs.readFileSync(`${ wikiWithNS }.all_titles.txt`, 'utf8').split(/[\n\r]+/);
+				out2 = [];
+				for (let i = 0; i < titles.length; i++) {
+					const t = titles[i].replace(/"/g, '\\"');
+					if (t) {
+						out2.push(`INSERT IGNORE INTO pages(title, prefix) VALUES("${ t }", "${ wiki }");`);
+					}
+				}
+				fs.writeFileSync(`${ wikiWithNS }.titles.sql`, out2.join("\n"));
+				console.log(`Generated sql import script @ ${ wikiWithNS }.titles.sql`);
+				return true;
+			}).catch(function(e) {
+				console.log(`Error: ${ e }`);
+			});
 		}).catch(function(e) {
 			console.log(`Error: ${ e }`);
 		});
--- server/scripts/testdb.info.js
+++ server/scripts/testdb.info.js
@@ -3,94 +3,27 @@
 
 module.exports = {
 	// How many titles do you want?
-	size: 190000,
+	size: 50000,
 
 	// How many of those do you want from traffic popularity
-	popular_pages_percentage: 2,
+	popular_pages_percentage: 5,
 
 	// How many of those do you want from the dumps?
 	// Rest will come from recent changes stream
-	dump_percentage: 75,
+	dump_percentage: 85,
 
 	wikis: [
 		// 1M+ wikipedias (ordered by depth)
-		{ prefix: 'enwiki',  limit: 60 },
-		{ prefix: 'frwiki',  limit: 20 },
-		{ prefix: 'eswiki',  limit: 20 },
-		{ prefix: 'ruwiki',  limit: 20 },
-		{ prefix: 'itwiki',  limit: 15 },
-		{ prefix: 'dewiki',  limit: 15 },
-		{ prefix: 'jawiki',  limit: 10 },
-		{ prefix: 'viwiki',  limit: 10 },
-		{ prefix: 'plwiki',  limit: 10 },
-		{ prefix: 'nlwiki',  limit: 10 },
-		{ prefix: 'svwiki',  limit: 8 },
-		{ prefix: 'warwiki', limit: 8 },
-		{ prefix: 'cebwiki', limit: 8 },
-
-		// Special wikis
-		{ prefix: 'commonswiki', limit: 6 },
-		{ prefix: 'metawiki', limit: 6 },
-
-		// 100K+ wikipedias (ordered by edits)
-		{ prefix: 'ptwiki',  limit: 6 },
-		{ prefix: 'zhwiki',  limit: 6 },
-		{ prefix: 'shwiki',  limit: 6 },
-		{ prefix: 'arwiki',  limit: 4 },
-		{ prefix: 'hewiki',  limit: 4 },
-		{ prefix: 'kowiki',  limit: 3 },
-		{ prefix: 'ukwiki',  limit: 3 },
-		{ prefix: 'trwiki',  limit: 2 },
-		{ prefix: 'huwiki',  limit: 2 },
-		{ prefix: 'cawiki',  limit: 2 },
-		{ prefix: 'nowiki',  limit: 2 },
-
-		// Other language wikipedias
-		{ prefix: 'zh_yuewiki',  limit: 2 },
-		{ prefix: 'thwiki',  limit: 2 },
-		{ prefix: 'hiwiki',  limit: 2 },
-		{ prefix: 'bnwiki',  limit: 2 },
-		{ prefix: 'mlwiki',  limit: 2 },
-
-		// link prefix languages
-		{ prefix: 'ckbwiki', limit: 1 },
-		{ prefix: 'cuwiki',  limit: 1 },
-		{ prefix: 'cvwiki',  limit: 1 },
-		{ prefix: 'hywiki',  limit: 1 },
-		{ prefix: 'iswiki',  limit: 1 },
-		{ prefix: 'kaawiki', limit: 1 },
-		{ prefix: 'kawiki',  limit: 1 },
-		{ prefix: 'lbewiki', limit: 1 },
-		{ prefix: 'lnwiki',  limit: 1 },
-		{ prefix: 'mznwiki', limit: 1 },
-		{ prefix: 'pnbwiki', limit: 1 },
-		{ prefix: 'uzwiki',  limit: 1 },
-
-		// wiktionary
-		{ prefix: 'enwiktionary', limit: 1 },
-		{ prefix: 'frwiktionary', limit: 1 },
-		{ prefix: 'itwiktionary', limit: 1 },
-		{ prefix: 'eswiktionary', limit: 1 },
-
-		// wikisource
-		{ prefix: 'enwikisource', limit: 1 },
-		{ prefix: 'frwikisource', limit: 1 },
-		{ prefix: 'itwikisource', limit: 1 },
-		{ prefix: 'eswikisource', limit: 1 },
-
-		// wikivoyage
-		{ prefix: 'enwikivoyage', limit: 1 },
-		{ prefix: 'frwikivoyage', limit: 1 },
-		{ prefix: 'itwikivoyage', limit: 1 },
-		{ prefix: 'eswikivoyage', limit: 1 },
-
-		// Talk namespaces from some wikis
-		{ prefix: 'enwiki',  ns: 1, limit: 5 },
-		{ prefix: 'arwiki',  ns: 1, limit: 3 },
-		{ prefix: 'dewiki',  ns: 1, limit: 3 },
-		{ prefix: 'ptwiki',  ns: 1, limit: 2 },
-		{ prefix: 'itwiki',  ns: 1, limit: 2 },
-		{ prefix: 'hewiki',  ns: 1, limit: 1 },
-		{ prefix: 'zhwiki',  ns: 1, limit: 1 },
+		{ prefix: 'enwiktionary',  limit: 60 },
+		{ prefix: 'frwiktionary',  limit: 20 },
+		{ prefix: 'eswiktionary',  limit: 20 },
+		{ prefix: 'ruwiktionary',  limit: 20 },
+		{ prefix: 'itwiktionary',  limit: 15 },
+		{ prefix: 'dewiktionary',  limit: 15 },
+		{ prefix: 'jawiktionary',  limit: 10 },
+		{ prefix: 'viwiktionary',  limit: 10 },
+		{ prefix: 'plwiktionary',  limit: 10 },
+		{ prefix: 'nlwiktionary',  limit: 10 },
+		{ prefix: 'svwiktionary',  limit: 18 },
 	],
 };
