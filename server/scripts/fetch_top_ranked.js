13c14977b7760258259a20be2e68f717fb6b9b63
C. Scott Ananian <cscott@cscott.net>
2026-02-19 02:10:20 -0500

    Update parsoidrendered.dblist and bump page metrics to 2026-01
    Change-Id: I5ee77791212845e67f0ff0110ceb920b761fce2b

--- server/scripts/fetch_top_ranked.js
+++ server/scripts/fetch_top_ranked.js
@@ -62,9 +62,9 @@ function runForWiki(prefix) {
 	if (month < 10) {
 		month = `0${ month }`;
 	}
-	// Hardcoding for now because 2025 data isn't available yet
-	year = 2024;
-	month = 12;
+	// Hardcoding for now (was 2024-12)
+	year = 2026;
+	month = '01';
 	console.log('Processing: ' + prefix + "; fetching: " + count + " items!");
 	const fetchArgs = {
 		prefix: prefix,
